57a5fdb8a58caebdf6a575f8feded66d22204b78
RaheemaMuhammed <121420610+RaheemaMuhammed@users.noreply.github.com>
2023-12-26 19:13:04 +0530

    error handling

--- frontend/src/Axios/instanse.js
+++ frontend/src/Axios/instanse.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 // export const axiosInstance=axios.create({baseURL:'http://127.0.0.1:8000/'})
-// export const axiosInstance=axios.create({baseURL:'http://3.27.212.135/'})
-export const axiosInstance=axios.create({baseURL:'https://authtasks.netlify.app/api/'})
+export const axiosInstance=axios.create({baseURL:'http://3.27.212.135/api/'})
+// export const axiosInstance=axios.create({baseURL:'https://authtasks.netlify.app/api/'})
 
